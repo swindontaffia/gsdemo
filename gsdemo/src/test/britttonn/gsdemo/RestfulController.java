bece5abf4ea6b2ac8343daeb5ef89707968e070d
snowjest <gitcommit@snowjest.plus.com>
2015-10-08 16:17:39 +0100

    Make the controller a restul one

--- gsdemo/src/test/britttonn/gsdemo/RestfulController.java
+++ gsdemo/src/test/britttonn/gsdemo/RestfulController.java
@@ -1,20 +1,29 @@
 package test.britttonn.gsdemo;
 
-import org.springframework.stereotype.Controller;
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import test.britttonn.gsdemo.model.beans.Sentence;
 
-@Controller
+@RestController
 public class RestfulController {
 
-    @RequestMapping(method=RequestMethod.GET, value="/gsdemo")
-    public String getSentence(@RequestParam(value="book") String bookName, @RequestParam(value="text") String text) {
+    @RequestMapping(method=GET, value="/gsdemo/findtext/json/{bookName}")
+    public Sentence getSentenceAsJson(@PathVariable String bookName, @RequestParam(value="text") String text) {
     	Sentence sentence = new Sentence();
-    	return sentence.addText("The sea glinted with merry making.").toString();
+    	return sentence.addText("The sea glinted with merry making");
     }
+    
+    @RequestMapping(method=GET, value="/gsdemo/findtext/text/{bookName}")
+    public String getSentenceAsText(@PathVariable String bookName, @RequestParam(value="text") String text) {
+    	Sentence sentence = new Sentence();
+    	return sentence.addText("The sea glinted with merry making").toString();
+    }
+
 }
 
 

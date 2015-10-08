bac1a95224486b42701cd61622649b753fc83d5e
snowjest <gitcommit@snowjest.plus.com>
2015-10-08 15:54:36 +0100

    Added basic controller & junit tests

--- gsdemo/src/test/britttonn/gsdemo/RestfulController.java
+++ gsdemo/src/test/britttonn/gsdemo/RestfulController.java
@@ -1,5 +1,20 @@
 package test.britttonn.gsdemo;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import test.britttonn.gsdemo.model.beans.Sentence;
+
+@Controller
 public class RestfulController {
 
+    @RequestMapping(method=RequestMethod.GET, value="/gsdemo")
+    public String getSentence(@RequestParam(value="book") String bookName, @RequestParam(value="text") String text) {
+    	Sentence sentence = new Sentence();
+    	return sentence.addText("The sea glinted with merry making.").toString();
+    }
 }
+
+

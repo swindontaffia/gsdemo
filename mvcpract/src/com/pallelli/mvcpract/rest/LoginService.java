190e41854d088c8608ed737b827aed80d5dc828f
snowjest <gitcommit@snowjest.plus.com>
2015-10-21 17:44:55 +0100

    Add login service junit test

--- mvcpract/src/com/pallelli/mvcpract/rest/LoginService.java
+++ mvcpract/src/com/pallelli/mvcpract/rest/LoginService.java
@@ -3,13 +3,11 @@ package com.pallelli.mvcpract.rest;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
-import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ResourceInfo;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
-import javax.ws.rs.core.SecurityContext;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -28,7 +26,7 @@ public class LoginService {
 	@AutorizationNotRequired
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
-	public Response doLogin(LoginForm loginForm, @Context ContainerRequestContext crc) {
+	public Response doLogin(LoginForm loginForm) {
 		
 		String [] roles = {"*"};
 		String token;
---
+++ mvcpract/test/com/pallelli/mvcpract/junit/LoginServiceTest.java
@@ -0,0 +1,56 @@
+package com.pallelli.mvcpract.junit;
+
+import static org.junit.Assert.*;
+import javax.ws.rs.core.Response;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.pallelli.mvcpract.rest.LoginForm;
+import com.pallelli.mvcpract.rest.LoginService;
+
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = { "/junit-app-context.xml" })
+
+public class LoginServiceTest {
+
+	@Autowired
+	private LoginService theService;
+	
+	@Test
+	public void test() {
+		testLoginOK();
+		testBadPassword();
+		testBadUserName();
+	}
+
+	private void testBadUserName() {
+		LoginForm loginForm = new LoginForm();
+		loginForm.setName("admn");
+		loginForm.setPassword("HJKII");
+		Response response = theService.doLogin(loginForm);
+		assertTrue(response.getStatus() == 401);
+		
+	}
+
+	private void testBadPassword() {
+		LoginForm loginForm = new LoginForm();
+		loginForm.setName("admin");
+		loginForm.setPassword("HJKI");
+		Response response = theService.doLogin(loginForm);
+		assertTrue(response.getStatus() == 401);
+	}
+
+	private void testLoginOK() {
+		LoginForm loginForm = new LoginForm();
+		loginForm.setName("admin");
+		loginForm.setPassword("HJKII");
+		Response response = theService.doLogin(loginForm);
+		assertTrue(response.getStatus() == 200);
+	}
+
+}
